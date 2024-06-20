2979e8ab562b80ee0d487db5bbbae05124395352
ukyo mashiko <ukmashi@winoo.co.jp>
2024-06-20 22:06:58 +0900

    temp save

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,27 @@
 import { MindMap } from "@/features/mindmap/components/maindamp";
+import { useState } from "react";
 
 const App: React.FC = () => {
+  const [fileName, setFileName] = useState('');
+  const ipcRenderer = (window as any).preload.ipcRenderer;
+
+  const handleOpneFile = () => {
+    console.log('ssssssssss');
+  }
 
   return(
     <>
-      <MindMap />
+      <h1>Hello world</h1>
+      <button onClick={handleOpneFile}>Open File</button>
+      <a
+        className="App-link"
+        onClick={() => ipcRenderer.send("openExternal", "https://reactjs.org")}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+      {/* <MindMap /> */}
     </>
   );
 };
