73948adafb1f7d8d2c0108aee37d9142b22be218
ukmashi <ukyo.mashiko@gmail.com>
2024-06-24 14:01:50 +0900

    add blocknote

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,12 @@
 import { useMemo } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { createRouter } from './routes';
-
 import { AppProvider } from './main-provider';
+
+import "@blocknote/core/fonts/inter.css";
+import { useCreateBlockNote } from "@blocknote/react";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
 
 const AppRouter = () => {
 
@@ -12,10 +16,14 @@ const AppRouter = () => {
 };
 
 function App() {
+  // Creates a new editor instance.
+  const editor = useCreateBlockNote();
+
   return (
     <AppProvider>
       {/* <AppRouter /> */}
       <h1>aaaaaaaa</h1>
+      <BlockNoteView editor={editor} />
     </AppProvider>
   );
 }
